c9c4980c98d704aca6f9cc7f48920e3c89bd666d
Admer2021 <77834265+Admer2021@users.noreply.github.com>
2023-11-24 18:55:28 +0100

    IB stranica code

---
+++ IB_stranica/index.js
@@ -0,0 +1,50 @@
+
+document.cookie = 'cookie1=value1; SameSite=Lax';
+
+document.cookie = 'cookie2=value2; SameSite=None; Secure';
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+function script() {
+    setTimeout(function () {
+        document.getElementById('elementToMove').style.left = '1000px';
+    }, 4000);
+}
+
+window.addEventListener(
+    "scroll",
+    () => {
+        document.body.style.setProperty(
+            "--scroll",
+            window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+        );
+    },
+    false
+);
